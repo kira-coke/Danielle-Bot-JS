e45e5ad6987bed3175337ca0019230a01ed406a3
kira-coke <44443096+kira-coke@users.noreply.github.com>
2024-06-21 15:39:02 +0000

    Work comman now works with a 10% bonus chance

--- work.js
+++ work.js
@@ -6,20 +6,37 @@ async function work(msg, userId){
   const userBal = await getUsersBalance(userId);
   const randomNumber = Math.floor(Math.random() * (1500 - 500 + 1)) + 500;
   const newBalance = userBal + randomNumber;
-  await saveUserBalance(userId, newBalance);
   function numberWithCommas(x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  const bonusNumber = Math.floor(Math.random() * 10) + 1; //change the 100 value to alter percentage of bonus
+  if(bonusNumber === 1){
+    const numberWithBonuses = randomNumber * 2;
+    const newBalanceWithBonuses = userBal+ numberWithBonuses;
+    const balanceFormated = numberWithCommas(newBalanceWithBonuses);
+
+    const balanceEmbed = new EmbedBuilder()
+        .setColor("#3498DB")
+        .setTitle(`Congrats! You worked extra hard and have earned double: ${Discord.inlineCode(numberWithCommas(numberWithBonuses))}`)
+        .setDescription(
+            "**New balance: **" + Discord.inlineCode(`${balanceFormated}`),
+        )
+        .setTimestamp();
+    msg.channel.send({ embeds: [balanceEmbed] });
+    await saveUserBalance(userId, newBalanceWithBonuses);
+    return;
   }
   const balWithCommas = numberWithCommas(newBalance);
 
   const balanceEmbed = new EmbedBuilder()
       .setColor("#ffa791")
-      .setTitle(`Congrats you worked hard and earned ${Discord.inlineCode(numberWithCommas(randomNumber))}`)
+      .setTitle(`You worked hard and earned: ${Discord.inlineCode(numberWithCommas(randomNumber))}`)
       .setDescription(
           "**New balance: **" + Discord.inlineCode(`${balWithCommas}`),
       )
       .setTimestamp();
-  msg.channel.send({ embeds: [balanceEmbed] });
+      msg.channel.send({ embeds: [balanceEmbed] });
+      await saveUserBalance(userId, newBalance);
   }
 
 module.exports = {work};
