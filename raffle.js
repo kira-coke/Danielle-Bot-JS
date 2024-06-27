a20951f20a21c4db4bc5c12a6bb8d290571bd31c
kira-coke <44443096+kira-coke@users.noreply.github.com>
2024-06-27 08:50:34 +0000

    Commented out raffle logic until its finished as breaks main bot

---
+++ raffle.js
@@ -0,0 +1,110 @@
+//const prizes = ['1', '2', '3', '4'];
+const emoteId = 1255801826108772362;
+const emote = `<:DB_currency:${emoteId}>`;
+const prizes = ['2'];
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, inlineCode} = require("discord.js");
+const {getRandomDynamoDBItem, changeNumberOwned, addToTotalCardCount, checkIfUserOwnsCard, writeToDynamoDB, getHowManyCopiesOwned} = require("./cards");
+const raffleEntries = new Set();
+
+async function forceRaffle(channel, client){
+    raffleEntries.clear();
+    raffle(channel, client);
+}
+
+async function raffle(channel, client){
+  const prize = prizes[Math.floor(Math.random() * prizes.length)];
+  const card = await getRandomDynamoDBItem('cards');
+  const amountOfCards = Math.floor(Math.random() * 4) + 1;
+  const amountOfCoins = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000;
+  const coinsWithCommas = numberWithCommas(amountOfCoins);
+  const embed = new EmbedBuilder()
+  .setTitle('Raffle Time!')
+  .setDescription('Click the button below to enter the raffle! You can only enter once.')
+  .setColor('Random');
+  if(prize ==='1'){//random amount of cards to win
+    console.log(card["card-id"]);
+    embed.addFields({name: "Enter for a chance to win: ", value: inlineCode(String(amountOfCards)+ " copies of " + card["card-id"]), inline: true})
+      .setImage(card["cardUrl"]);
+      
+  }
+  if(prize === '2'){
+    console.log(amountOfCoins);
+      embed.addFields({name: "Enter for a chance to win: ", value: inlineCode(String(coinsWithCommas))+ `${emote}`, inline: true})
+  }
+  if(prize === '3'){
+
+  }
+  if(prize === '4'){
+
+  }
+  const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+          .setCustomId('raffle_entry')
+          .setLabel('Enter Raffle')
+          .setStyle('Primary')
+  );
+
+  const message = await channel.send({ embeds: [embed], components: [row] });
+
+  const filter = i => i.customId === 'raffle_entry';
+  const collector = message.createMessageComponentCollector({ filter, time: 0.1 * 60 * 1000 }); //change back to 0.5
+
+  collector.on('collect', async interaction => {
+      if (raffleEntries.has(interaction.user.id)) {
+          await interaction.reply({ content: 'You have already entered the raffle.', ephemeral: true });
+      } else {
+          raffleEntries.add(interaction.user.id);
+          await interaction.reply({ content: 'Your entry has been noted.', ephemeral: true });
+      }
+  });
+  collector.on('end', () => {
+      if (raffleEntries.size > 0) {
+        const winnerId = Array.from(raffleEntries)[Math.floor(Math.random() * raffleEntries.size)];
+        const winner = client.users.cache.get(winnerId);
+        if(prize === '1'){
+            cardWin(winnerId, amountOfCards, card);
+        }
+        if(prize === '2'){
+            return;
+        }
+      channel.send(`Congratulations ${winner}!`);
+      raffleEntries.clear();
+      } else {
+          channel.send('No entries for this raffle.');
+      }
+  });
+}
+
+async function cardWin(winnerId, amount, card){
+    const owned = await checkIfUserOwnsCard('user-cards', winnerId, card["card-id"]);
+    let userOwns = await getHowManyCopiesOwned('user-cards', winnerId, card["card-id"]);
+    if(owned === 0){
+        const item = {
+            "user-id": winnerId, //primary key
+            "card-id": card["card-id"], //secondary key
+            "copies-owned": amount,
+            exp: 0,
+            level: 0,
+            upgradable: false,
+            tier: 1,
+            totalExp: 0
+        }
+        await writeToDynamoDB('user-cards', item)
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+        userOwns = 1;
+        if(amount > 1){
+            await changeNumberOwned("user-cards", winnerId, card["card-id"], parseInt(userOwns) + (amount-1));
+        }
+        await addToTotalCardCount("Dani-bot-playerbase", winnerId, parseInt(userOwns)+ amount)
+    }else{
+        await changeNumberOwned("user-cards", winnerId, card["card-id"], parseInt(userOwns) + amount);
+    }
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+module.exports = {forceRaffle, raffle};
