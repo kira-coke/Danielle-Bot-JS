defa756633c3100d614431d83609969535d4571a
kira-coke <44443096+kira-coke@users.noreply.github.com>
2024-06-27 11:37:01 +0000

    Inv and index now both should work with a group given and by themselves

--- inventory.js
+++ inventory.js
@@ -7,6 +7,64 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient
 
 async function generateEmbedInv(page, totalPages, listOfCards, msg, userId) {
+
+    let user;
+    try {
+        user = await msg.client.users.fetch(userId);
+    } catch (error) {
+        console.error("Failed to fetch user:", error); // Log error if fetching fails
+        throw error;
+    }
+    const embed = new EmbedBuilder()
+        .setTitle(
+            `Displaying ${user.username}'s inventory                                        (Page ${page + 1}/${totalPages})`,
+        )
+        .setColor("#feb69e")
+        .setFooter({
+            text: msg.author.tag,
+            iconURL: msg.author.displayAvatarURL({
+                dynamic: true,
+            }),
+        });
+
+    const cardsPerPage = 4; // Ensure this matches the value in the index constant too otherwise will end up with extra blank pages
+    const startIndex = page * cardsPerPage;
+    const endIndex = Math.min(
+        startIndex + cardsPerPage,
+        listOfCards.length,
+    );
+
+    const cardSubset = listOfCards.slice(startIndex, endIndex);
+
+    for (const attribute of cardSubset) {
+        try {
+            const card = await getCardFromTable("cards", attribute["card-id"]);
+
+            const ownsCard = await checkIfUserOwnsCard("user-cards", userId, attribute["card-id"]);
+
+            if (ownsCard !== 0) {
+                const cardDataArray = await getUserCard("user-cards", userId, attribute["card-id"]);
+                const cardData = cardDataArray[0];
+                embed.addFields(
+                    { 
+                        name: "\u200B", 
+                        value: `${Discord.blockQuote(Discord.bold(String(card["GroupMember"])))} (${Discord.bold(String(card["Theme"]))}) ${Discord.inlineCode(String(cardData.exp) + "/100")} | ${Discord.inlineCode("Lvl." + String(cardData.level))} | ${Discord.inlineCode(String(cardData["copies-owned"]))}`, 
+                        inline: false 
+                    }
+                );
+            } else {
+                //console.log("User does not own: " + attribute["card-id"]);
+            }
+        } catch (error) {
+            console.error("Error processing card:", error);
+            // Optionally handle error or continue with next card
+        }
+    }
+
+    return embed;
+};
+
+async function generateEmbedInvForGroup(page, totalPages, listOfCards, msg, userId) {
   
     let user;
     try {
@@ -64,7 +122,6 @@ async function generateEmbedInv(page, totalPages, listOfCards, msg, userId) {
             }
         } catch (error) {
             console.error("Error processing card:", error);
-            // Optionally handle error or continue with next card
         }
     }
 
@@ -91,7 +148,7 @@ const handleCollectorInv = (embedMessage, msg, totalPages, listOfCards, userId) 
     const filter = (i) => i.user.id === msg.author.id;
     const collector = embedMessage.createMessageComponentCollector({
         filter,
-        time: 30000, // How long buttons last
+        time: 60000, // How long buttons last
     });
 
     collector.on("collect", async (i) => {
@@ -112,6 +169,33 @@ const handleCollectorInv = (embedMessage, msg, totalPages, listOfCards, userId) 
     });
 };
 
+const handleCollectorInvForGroup = (embedMessage, msg, totalPages, listOfCards, userId) => {
+    let currentPage = 0;
+    const filter = (i) => i.user.id === msg.author.id;
+    const collector = embedMessage.createMessageComponentCollector({
+        filter,
+        time: 30000, // How long buttons last
+    });
+
+    collector.on("collect", async (i) => {
+        await i.deferUpdate();
+        if (i.customId === "prev" && currentPage > 0) {
+            currentPage--;
+        } else if (i.customId === "next" && currentPage < totalPages - 1) {
+            currentPage++;
+        }
+        await embedMessage.edit({
+            embeds: [await generateEmbedInvForGroup(currentPage, totalPages, listOfCards, msg, userId)],
+            components: [generateRowInv(currentPage, totalPages)],
+        });
+    });
+
+    collector.on("end", (collected) => {
+        embedMessage.edit({ components: [] });
+    });
+};
+
+
 async function getUniqueGroupNames(tableName) {
     let params = {
         TableName: tableName,
@@ -119,8 +203,6 @@ async function getUniqueGroupNames(tableName) {
     };
 
     let uniqueGroupNames = new Set();
-
-    // Function to scan the DynamoDB table and collect unique groupNames
     async function scanDynamoDB(lastEvaluatedKey = null) {
         if (lastEvaluatedKey) {
             params.ExclusiveStartKey = lastEvaluatedKey;
@@ -146,4 +228,4 @@ async function getUniqueGroupNames(tableName) {
     await scanDynamoDB();
     return Array.from(uniqueGroupNames);
 }
-module.exports = { generateEmbedInv, generateRowInv, handleCollectorInv, getUniqueGroupNames };
+module.exports = { generateEmbedInv, generateEmbedInvForGroup, generateRowInv, handleCollectorInv, getUniqueGroupNames, handleCollectorInvForGroup  };
